286db1ecd067bfa09af7a1b1c61b435c71cdf4ce
nico <ainililia@.com>
2018-09-14 18:00:00 +0800

    up

---
+++ src/main/java/org/nico/ourbatis/el/Noel.java
@@ -0,0 +1,13 @@
+package org.nico.ourbatis.el;
+
+public class Noel {
+	
+	private String prefix;
+	
+	private String suffix;
+
+	public Noel(String prefix, String suffix) {
+		this.prefix = prefix;
+		this.suffix = suffix;
+	}
+}
---
+++ src/main/java/org/nico/ourbatis/el/NoelAssist.java
@@ -0,0 +1,6 @@
+package org.nico.ourbatis.el;
+
+public class NoelAssist {
+
+	
+}
